377b4e3eaf8260e98549da6bfd90126b8cc09411
Aafnan M <aafnanm13@gmail.com>
2024-02-03 15:40:54 -0500

    Fixed several bugs preventing the server from running correctly

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,9 +1,9 @@
-from django import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('',views.home, name= 'home'),
-    path('home', views.home, name= 'home')
-    path('stats', views.stats, name = 'stats'),
+    path('home', views.home, name= 'home'),
+    path('stats', views.stats, name= 'stats'),
     path('sign-up', views.sign_up, name= 'sign_up'),
     ]
