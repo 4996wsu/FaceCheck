3461ef0df89c67d012cd1bc8a12aa52bf23f2b9d
mhachem <mhachem99@gmail.com>
2024-02-03 15:05:10 -0500

    created forms.py and sign_up.html

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.home, name= 'home'),
     path('home', views.home, name= 'home')
     path('stats', views.stats, name = 'stats'),
-]
+    path('sign-up', views.sign_up, name= 'sign_up'),
+    ]
