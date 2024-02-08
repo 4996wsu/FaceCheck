f0c26bd9831371ce6dfcb0ad24bc349615b927fe
Aafnan M <aafnanm13@gmail.com>
2024-02-07 20:41:06 -0500

    Configured static, added logo, added brief about section

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',views.home, name= 'home'),
     path('home', views.home, name= 'home'),
     path('stats', views.stats, name= 'stats'),
     path('sign-up', views.sign_up, name= 'sign_up'),
-    ]
+]
+
+urlpatterns += staticfiles_urlpatterns()
