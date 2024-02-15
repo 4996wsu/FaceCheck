4ac7dc54c0f29bb15506e7e285baa5923538f468
Mohamad <mhachem99@gmail.com>
2024-02-15 13:16:55 -0500

    Changes to Login

--- webapp/main/forms.py
+++ webapp/main/forms.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
+    username = forms.CharField(
+        label='Username',
+        max_length=20,
+        help_text='Required. 20 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     class Meta:
         model = User
         fields = ["username","email","password1", "password2"]
--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',views.home, name= 'home'),
@@ -7,3 +8,5 @@ urlpatterns = [
     path('stats', views.stats, name= 'stats'),
     path('sign-up', views.sign_up, name= 'sign_up'),
     ]
+
+urlpatterns += staticfiles_urlpatterns()
