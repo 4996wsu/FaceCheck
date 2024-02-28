c02ba3ed264ca22de0dbbaf97d32b48abeb1ffc6
Mohamad <mhachem99@gmail.com>
2024-02-28 17:23:19 -0500

    Style changes to the whole web app

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('',views.home, name= 'home'),
     path('home', views.home, name= 'home'),
     path('stats', views.stats, name= 'stats'),
     path('sign-up', views.sign_up, name= 'sign_up'),
+    path('logout/', LogoutView.as_view(next_page='/home'), name='logout'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
