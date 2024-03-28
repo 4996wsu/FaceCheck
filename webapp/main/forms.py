671ebfe9f311720ca2bafeba0af4ab1fbdf0a7fd
Aafnan M <aafnanm13@gmail.com>
2024-03-28 15:17:30 -0400

    Fixed login changes

--- webapp/main/forms.py
+++ webapp/main/forms.py
@@ -4,26 +4,48 @@ from django.contrib.auth.models import User
 from .models import Post
 from django.core.exceptions import ValidationError
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class RegisterForm(forms.ModelForm):
+    email = forms.EmailField(required=False)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput, required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
-    username = forms.CharField(
-        label='Access ID',
-        max_length=8,
-        help_text='Required. 8 characters or fewer. Letters, digits and @/./+/-/_ only.',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
+
     class Meta:
         model = User
-        fields = ["username","email","first_name","last_name","password1", "password2"]
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise ValidationError('Email already exists')
-        if not email.endswith('@wayne.edu'):
-            raise ValidationError('You must use a wayne.edu email address')
-        return email
+        fields = ['username', 'email', 'first_name', 'last_name']
+        
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = 'Access ID'
+        self.fields['username'].widget.attrs['placeholder'] = 'Ex: hc9082'
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Passwords do not match.')
+
+        return password2
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if not first_name:
+            self.add_error('first_name', 'First name cannot be blank.')
+        
+        if not last_name:
+            self.add_error('last_name', 'Last name cannot be blank.')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
 
 class ImageUploadForm(forms.Form):
     image = forms.ImageField(label='Upload your photo', label_suffix='')
